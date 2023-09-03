cda18ffe9e1defa63660e0526291fbf0b6cd1353
jossduff <jduff360@gmail.com>
2023-09-03 13:18:24 -0400

    Gonna try something

--- src/data_updater.rs
+++ src/data_updater.rs
@@ -11,7 +11,7 @@ use std::collections::HashMap;
 use tokio::sync::mpsc::{channel, Receiver, Sender};
 
 pub struct DataUpdater {
-    data_from_indexer: Receiver<Address>,
+    receiver_from_indexer: Receiver<Address>,
     sender_to_database_manager: Sender<Command>,
     comptroller: Comptroller<Provider<Ws>>,
     price_oracle: PriceOracle,
@@ -19,13 +19,13 @@ pub struct DataUpdater {
 
 impl DataUpdater {
     pub fn new(
-        data_from_indexer: Receiver<Address>,
+        receiver_from_indexer: Receiver<Address>,
         sender_to_database_manager: Sender<Command>,
         comptroller: Comptroller<Provider<Ws>>,
     ) -> DataUpdater {
         let price_oracle = PriceOracle::new();
         DataUpdater {
-            data_from_indexer,
+            receiver_from_indexer,
             sender_to_database_manager,
             comptroller,
             price_oracle,
@@ -35,21 +35,51 @@ impl DataUpdater {
     pub async fn run(&mut self) -> () {
         println!("Data_updater is running");
 
+        // Clone the necessary variables for each task
+        let receiver_from_indexer_clone = self.receiver_from_indexer.take().expect("no receiver");
+        let sender_to_database_manager_clone = self.sender_to_database_manager.clone();
+        let comptroller_clone = self.comptroller.clone();
+        let price_oracle_clone = self.price_oracle.clone();
+
         // start add_new_asset task
+        let new_account_task = tokio::spawn(async move {
+            let _ = Self::watch_for_new_accounts(
+                receiver_from_indexer_clone,
+                sender_to_database_manager_clone,
+                comptroller_clone,
+                price_oracle_clone,
+            )
+            .await;
+        });
+
+        let sender_to_database_manager_clone = self.sender_to_database_manager.clone();
+        let comptroller_clone = self.comptroller.clone();
+        let price_oracle_clone = self.price_oracle.clone();
 
         // start update_data task
+        let update_data_task = tokio::spawn(async move {
+            let _ = Self::update_all_data(
+                sender_to_database_manager_clone,
+                comptroller_clone,
+                price_oracle_clone,
+            )
+            .await;
+        });
     }
 
-    async fn watch_for_new_accounts(&mut self) {
-        while let Some(address) = self.data_from_indexer.recv().await {
-            // call comptroller functions
-            // call ctoken functions
-
-            //
-        }
+    async fn watch_for_new_accounts(
+        receiver_from_indexer: Receiver<Address>,
+        sender_to_database_manager: Sender<Command>,
+        comptroller: Comptroller<Provider<Ws>>,
+        price_oracle: PriceOracle,
+    ) {
     }
 
-    async fn update_all_data() {
+    async fn update_all_data(
+        sender_to_database_manager: Sender<Command>,
+        comptroller: Comptroller<Provider<Ws>>,
+        price_oracle: PriceOracle,
+    ) {
         // get all accounts from db
         // for each account, update data
         // save updated data to db
