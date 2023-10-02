77cf67380694c735d773d93b71a42e4e77b6bb6e
jossduff <jduff360@gmail.com>
2023-10-02 17:51:52 -0400

    Cleaned up everything except account discovery functions

--- src/indexer.rs
+++ src/indexer.rs
@@ -28,26 +28,29 @@ const STEP_SIZE: u64 = 500_000;
 
 pub struct Indexer {
     client: Arc<Provider<Http>>,
+    database: Database,
     comptroller_creation_block: u64,
-    comptroller_instance: Arc<generated::Comptroller<Provider<Http>>>,
+    comptroller_instance: Arc<generated::Comptroller<Provider<Http>>>, // TODO: is it best practice to share these instances?  Or should I just initialize each time?
     ctoken_instances: Arc<HashMap<Address, CErc20<Provider<Http>>>>,
 }
 
 impl Indexer {
     pub fn new(
-        ethers_client: Arc<Provider<Http>>,
+        client: Arc<Provider<Http>>,
         comptroller_address: Address,
         comptroller_creation_block: u64,
     ) -> Indexer {
+        let database = Database::new().unwrap();
         let comptroller_instance: Arc<generated::Comptroller<Provider<Http>>> = Arc::new(
-            generated::Comptroller::new(comptroller_address, ethers_client.clone()),
+            generated::Comptroller::new(comptroller_address, client.clone()),
         );
         // TODO: is it bad practice for this to be un-initialized?
         let ctoken_instances: Arc<HashMap<Address, CErc20<Provider<Http>>>> =
             Arc::new(HashMap::new());
 
         Indexer {
-            client: ethers_client,
+            client,
+            database,
             comptroller_creation_block,
             comptroller_instance,
             ctoken_instances,
@@ -57,46 +60,11 @@ impl Indexer {
     pub async fn run(&mut self) {
         println!("Indexer::run()");
 
-        let mut database = Database::new().unwrap();
-        let mut addresses_to_watch: Vec<Address> = Vec::new();
-        addresses_to_watch.push(self.comptroller_instance.address());
+        self.db_initialize_comptroller().await;
+        self.db_initialize_all_ctokens().await;
 
-        self.build_initial_db_comptroller(&mut database).await;
-
-        let all_ctoken_addresses: Vec<Address> = self
-            .comptroller_instance
-            .get_all_markets()
-            .call()
-            .await
-            .unwrap();
-        println!(
-            "Got all {} ctokens from comptroller.getAllMarkets()",
-            all_ctoken_addresses.len()
-        );
-
-        // TODO: maybe move this inside new()?
-        let mut ctoken_instances: HashMap<Address, CErc20<Provider<Http>>> = HashMap::new();
-        for ctoken_address in all_ctoken_addresses {
-            addresses_to_watch.push(self.comptroller_instance.address());
-            let ctoken_instance: CErc20<Provider<Http>> =
-                CErc20::new(ctoken_address, self.client.clone());
-            self.build_initial_db_ctoken(&ctoken_instance, &mut database)
-                .await;
-            ctoken_instances.insert(ctoken_address, ctoken_instance);
-        }
-
-        self.ctoken_instances = Arc::new(ctoken_instances);
-
-        // Ok
-        // we're going to sequentially run 2 processes
-        // first, from comptroller creation block to bot start block watch all market enter/exit to get a list of all accounts
-        // then do a query every 10 blocks for ALL of the other events we need to handle
         let bot_start_block = self.client.get_block_number().await.unwrap().as_u64();
-        let reading_start_block: u64 = self.comptroller_creation_block;
-        // this pass of past events just finds marketenter/exit events to build a list of all active accounts
-        // to initialize the database
-        self.read_past_events(reading_start_block, bot_start_block)
-            .await;
+        self.discover_accounts(bot_start_block).await;
         self.read_current_events(bot_start_block).await;
     }
 
@@ -141,7 +109,7 @@ impl Indexer {
         }
     }
 
-    async fn read_past_events(&mut self, start_block: u64, end_block: u64) {
+    async fn discover_accounts(&mut self, end_block: u64) {
         let comptroller_events: Vec<&str> = vec![
             "MarketEntered(address,address)",
             "MarketExited(address,address)",
@@ -152,11 +120,12 @@ impl Indexer {
         // address account to set of ctokens in
         let mut account_ctokens_in: HashMap<Address, HashSet<Address>> = HashMap::new();
         let mut step_size = STEP_SIZE;
-        let mut i = start_block;
+        // start at comptroller_creation_block
+        let mut i = self.comptroller_creation_block;
         // let mut temp_total_events: u64 = 0;
         let mut last_run_failure: bool = false;
         while i <= end_block {
-            print_progress_percent(i, start_block, end_block);
+            print_progress_percent(i, self.comptroller_creation_block, end_block);
 
             let comptroller_filters: Vec<Filter> = comptroller_events
                 .iter()
@@ -269,20 +238,43 @@ impl Indexer {
         // println!("db up to date");
     }
 
-    // make initial calls for ctokens: underlyingAddress, exchangeRateStored
+    async fn db_initialize_all_ctokens(&mut self) {
+        let all_ctoken_addresses: Vec<Address> = self
+            .comptroller_instance
+            .get_all_markets()
+            .call()
+            .await
+            .unwrap();
+        println!(
+            "Got all {} ctokens from comptroller.getAllMarkets()",
+            all_ctoken_addresses.len()
+        );
+
+        let mut ctoken_instances: HashMap<Address, CErc20<Provider<Http>>> = HashMap::new();
+        for ctoken_address in all_ctoken_addresses {
+            let ctoken_instance: CErc20<Provider<Http>> =
+                CErc20::new(ctoken_address, self.client.clone());
+            self.db_initialize_single_ctoken(&ctoken_instance).await;
+            ctoken_instances.insert(ctoken_address, ctoken_instance);
+        }
+
+        self.ctoken_instances = Arc::new(ctoken_instances);
+    }
+
+    // make initial calls for ctoken: underlyingAddress, exchangeRateStored
     // also comptroller.markets(ctoken) for collateral factor
     // create ctoken in DB
-    async fn build_initial_db_ctoken(
-        &mut self,
-        ctoken_instance: &CErc20<Provider<Http>>,
-        database: &mut Database,
-    ) {
+    async fn db_initialize_single_ctoken(&mut self, ctoken_instance: &CErc20<Provider<Http>>) {
         // TODO: put these in multicalls
         // TODO: error on ceth calls.
         let underlying_address: Address = match ctoken_instance.underlying().call().await {
             Ok(x) => x,
             Err(err) => {
-                println!("contract doesn't exist: {}", ctoken_instance.address());
+                println!(
+                    "contract doesn't exist: {} \n error: {}",
+                    ctoken_instance.address(),
+                    err
+                );
                 return;
             }
         };
@@ -309,31 +301,27 @@ impl Indexer {
         );
         let db_key: DBKey = DBKey::CToken(ctoken_instance.address());
         let db_val: DBVal = DBVal::CToken(new_ctoken);
-        database.set(&db_key, &db_val);
+        self.database.set(&db_key, &db_val);
     }
 
     // make comptroller instance
     // initial comptroller calls:
     // closeFactor
     // liquidationIncentive
-    async fn build_initial_db_comptroller(&mut self, database: &mut Database) {
+    async fn db_initialize_comptroller(&mut self) {
         let close_factor_mantissa: U256 = self
             .comptroller_instance
             .close_factor_mantissa()
             .call()
             .await
             .unwrap();
-        // ex: close_factor_mantissa = 500,000,000,000,000,000 -> close_factor = 0.5
-        // let close_factor: f64 = close_factor_mantissa.as_u64() as f64 / ONE_ETHER_IN_WEI as f64;
+
         let liquidation_incentive_mantissa: U256 = self
             .comptroller_instance
             .liquidation_incentive_mantissa()
             .call()
             .await
             .unwrap();
-        // ex: liquidation_incentive_mantissa = 1,080,000,000,000,000,000 -> liquidation_incentive = 1.08
-        // let liquidation_incentive: f64 =
-        //     liquidation_incentive_mantissa.as_u64() as f64 / ONE_ETHER_IN_WEI as f64;
 
         // set comptroller in DB
         let new_comptroller = Comptroller::new(
@@ -343,7 +331,7 @@ impl Indexer {
         );
         let db_key: DBKey = DBKey::Comptroller();
         let db_val: DBVal = DBVal::Comptroller(new_comptroller);
-        database.set(&db_key, &db_val);
+        self.database.set(&db_key, &db_val);
     }
 }
 
