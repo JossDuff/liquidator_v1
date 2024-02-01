726f308f5426e84bbe113e967dc2774f3de5626e
jossduff <jduff360@gmail.com>
2024-01-31 22:11:00 -0500

    fresh start

--- src/types/target_events.rs
+++
@@ -1,7 +0,0 @@
-use crate::bindings::c_erc20_bindings::CErc20Events;
-use crate::bindings::comptroller_bindings::ComptrollerEvents;
-
-pub enum TargetEvents {
-    ComptrollerEvent(ComptrollerEvents),
-    CTokenEvent(CErc20Events),
-}
